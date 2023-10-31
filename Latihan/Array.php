fe293b9c6196e336e7ecd75a28af0db9a41ad45d
iMnuelll <imanueluneputty121103@gmail.com>
2023-11-01 06:43:46 +0700

    W1

--- Latihan/Array.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Contoh  penggunaan Array
-$cars = array("Ferrary", "Lamborgini Aventador", "Audi");
-$negara =["Singapura", "Indonesia", "Jepang"];
-echo"Ini contoh Array : \n";
-echo "I like". $cars[0].", ".$cars[1]." and ".$cars[2]. ".";
-// var_dump($negara);
-?>
